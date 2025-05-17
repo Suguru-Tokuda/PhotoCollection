f47a8c9f3b0a854fe082c33874cb90d30ef45cb1
Suguru Tokuda <suguru.tokuda@gmail.com>
2025-05-17 10:10:41 -0500

    Initial Commit

---
+++ SwiftConcurrencyDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftConcurrencyDemo
+//
+//  Created by Suguru Tokuda on 5/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftConcurrencyDemoTests/SwiftConcurrencyDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyDemoTests.swift
+//  SwiftConcurrencyDemoTests
+//
+//  Created by Suguru Tokuda on 5/17/25.
+//
+
+import Testing
+@testable import SwiftConcurrencyDemo
+
+struct SwiftConcurrencyDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
